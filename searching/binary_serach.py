57600d24f3440300871b0ee434ea82843a124b29
rajendraprasad10 <rajendra.taidala10@gmail.com>
2025-12-10 13:33:10 +0530

    added jenkins ci and pipfile init

--- searching/binary_serach.py
+++ searching/binary_serach.py
@@ -2,28 +2,36 @@
 
 # [2,3,4,5,6]
 
-
-def binary_search(lst,target):
-    size = len(lst)
-    start = 0 
-    end = size - 1
+def binary_search(lst, target):
+    """
+    Iterative binary search.
+    Returns the index of `target` in `lst`, or -1 if not found.
+    Assumes `lst` is sorted in ascending order.
+    
+    """
+    
+    start = 0
+    end = len(lst) - 1
     print("===========")
-    while(start <= end):
-        mid = (start + end ) // 2  
+    while start <= end:
+        mid = (start + end) // 2
 
-        print(mid)
+        print("mid:", mid, "start:", start, "end:", end)
         if lst[mid] == target:
             return mid
-        
+
+        # Search left
         elif lst[mid] > target:
             print("====", lst[mid])
             end = mid - 1
-        
-        elif lst[mid] < target:
+
+        # Search right
+        else:
             print(";;;", lst[mid])
-            start = mid + 1 
+            start = mid + 1
 
-    return -1 
-    
+    return -1
 
-print(binary_search([2,3,4,5,6,7], 7))
+
+if __name__ == "__main__":
+    print(binary_search([2,3,4,5,6,7], 6))
